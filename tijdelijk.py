6d490f8b0e06f0c7ceaeab2e05859b100f8770c1
Fons Lauwers <mlauwers1958@gmail.com>
2024-09-28 20:38:55 +0200

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,34 +1,43 @@
-# dictionary prijzen aangemaakt
-prijzen = {"aardbei" : 3,
-           "vanille" : 4,
-           "chocolade" : 5
-           }
+from helper import decoreer
 
-# formule aanbieding prijs aardbei maal 0.8
-aanbieding = prijzen["aardbei"] * 0.8
+def print_aanbieding():
+   # dictionary prijzen aangemaakt
+   prijzen = {"aardbei" : 3,
+            "vanille" : 4,
+            "chocolade" : 5
+            }
 
-# reclametekst met formatted string met de formule aanbieding
-reclame_tekst =f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}"
+   # formule aanbieding prijs aardbei maal 0.8
+   aanbieding = prijzen["aardbei"] * 0.8
 
-# reclametekst prijs aangepast tot 2 decimalen achter de komma
-reclame_tekst2 = reclame_tekst [:63]
+   # reclametekst met formatted string met de formule aanbieding
+   reclame_tekst =f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}"
 
-# in hoofdletters geprint
-reclame_tekst3 = reclame_tekst2.upper()
+   # reclametekst prijs aangepast tot 2 decimalen achter de komma
+   reclame_tekst2 = reclame_tekst [:63]
 
-# reclametekst voor drukker in een list van alle woorden
-reclame_tekst4 = reclame_tekst3.split()
+   # in hoofdletters geprint
+   reclame_tekst3 = reclame_tekst2.upper()
 
-'''
-reclametekst onder elkaar, waarbij elementen die vijf of meer karakters hebben in hoofdletters
-minder dan 5 in kleine letters
-'''  
-for el in reclame_tekst4:
- if (len(el)) < 5:
+   # reclametekst voor drukker in een list van alle woorden
+   reclame_tekst4 = reclame_tekst3.split()
 
-    print(el.lower())
- else:
-    print(el.upper())
+   '''
+   reclametekst onder elkaar, waarbij elementen die vijf of meer karakters hebben in hoofdletters
+   minder dan 5 in kleine letters
+   '''  
+   for el in reclame_tekst4:
+     if  (len(el)) < 5:
+
+      print(el.lower())
+     else:
+      print(el.upper())
+
+decoreer("Aanbieding")
+print_aanbieding()
+
+
+
     
 
 
